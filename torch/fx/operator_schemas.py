ffd1e64291b28f5367ae89170fa5c0a0e339d8c3
Riley Dulin <dulinr@meta.com>
2025-04-21 14:52:47 -0700

    [torch][fx] Add support for EXIR dialect overload ops in normalize_function (#143689)
    Summary:
    
    I had a minor annoyance when debugging graphs using EXIR dialect ops,
    that all the function normalization went away. For functions with > 5 arguments,
    some of which are just simple bools and ints, it's very helpful to have
    the kwarg names attached.
    
    Enhance `normalize_target` to handle EdgeOpOverload targets. To avoid
    a circular dependency on Executorch from pytorch core, I just use a `hasattr`
    check for "_op". This only happens if the target is not already a recognized
    torch function.
    
    Also, I noticed that the new `fx.Node.normalized_arguments` function
    didn't forward an important kwarg to `normalize_target`, so I fixed that too.
    
    Test Plan: Tested with FxGraphDrawer and an fx Graph containing EXIR nodes.
    
    Differential Revision: D67545909

--- torch/fx/operator_schemas.py
+++ torch/fx/operator_schemas.py
@@ -368,6 +368,16 @@ def normalize_function(
     new_args_and_kwargs = None
     if not isinstance(target, types.BuiltinFunctionType) and not (
         isinstance(target, (OpOverloadPacket, OpOverload))
+    ) and hasattr(target, "_op"):
+        # ExecuTorch's EdgeOpOverload are a wrapper around PyTorch's OpOverload,
+        # so we can unwrap it here to get its schema
+        # Can't import EdgeOpOverload directly because of a circular dependency,
+        # so checking for "_op" existing is the next best thing.
+        target = target._op
+
+    # Repeat the condition after checking for the inner _op field.
+    if not isinstance(target, types.BuiltinFunctionType) and not (
+        isinstance(target, (OpOverloadPacket, OpOverload))
     ):
         target_for_analysis = target
         if target in boolean_dispatched:
